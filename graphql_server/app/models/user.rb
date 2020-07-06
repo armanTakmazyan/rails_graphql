5948b601cc8f7d5fabc54cd86814c18d28cb5dab
Arman Takmazyan <armanworkmail01@gmail.com>
2020-07-06 18:28:43 -0400

    Added a json_web_token gem. Changed the has_many field of User model.

--- graphql_server/app/models/user.rb
+++ graphql_server/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
-    has_many :refresh_token, dependent: :destroy 
+    has_many :refresh_tokens, dependent: :destroy 
 
     validates :name, presence: true, length: { minimum: 2, maximum: 100 }
     validates :email, presence: true, length: { maximum: 100 }, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'Requires a valid email format' }, uniqueness: true
